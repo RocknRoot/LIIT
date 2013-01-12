fadaed770afa60a6ff546da1780d49b68de907fd
Thibaut Deloffre <tib@rocknroot.org>
2013-01-12 22:42:47 +0100

    Add admin routes

--- LIIT/settings.py
+++ LIIT/settings.py
@@ -110,7 +110,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'issues'
+    'admin',
+    'issues',
 )
 
 # A sample logging configuration. The only tangible logging
--- LIIT/urls.py
+++ LIIT/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+import admin.urls
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'LIIT.views.home', name='home'),
     # url(r'^LIIT/', include('LIIT.foo.urls')),
-
+    url(r'^admin/', include(admin.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
