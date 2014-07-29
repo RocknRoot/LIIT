676d784dbfef3c6dc25403945f1d35f0cc6dbc94
Valerian Cubero <nekith@gmail.com>
2014-07-30 00:01:56 +0200

    #32 Issue model

--- incident/models.py
+++ incident/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+class Issue(models.Model):
+    title = models.CharField(_('Title'), max_length=150)
+    description = models.TextField(_('Description'))
+    contract = models.ForeignKey('structure.Contract')
+    assigned_team = models.ForeignKey('structure.Team')
+    assigned_user = models.ForeignKey('structure.User', null=True)
