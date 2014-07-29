f71cf4a8c7741e5c5dfa50f6d2d6a1e09d10d01f
Valerian Cubero <nekith@gmail.com>
2014-07-29 23:52:07 +0200

    Add verbose names and meta to structure models

--- structure/models.py
+++ structure/models.py
@@ -28,7 +28,17 @@ class ContractOrganization(models.Model):
     contract = models.ForeignKey('Contract')
     organization = models.ForeignKey('Organization')
     default_team = models.ForeignKey('Team')
+    def __unicode__(self):
+        return self.contract.name + ' - ' + self.organization.name
+    class Meta:
+        verbose_name = _('Contracted organization')
+        verbose_name_plural = _('Contracted organizations')
 
 class ContractTeam(models.Model):
     contract = models.ForeignKey('Contract')
     team = models.ForeignKey('Team')
+    def __unicode__(self):
+        return self.contract.name + ' - ' + self.team.name
+    class Meta:
+        verbose_name = _('Contracted team')
+        verbose_name_plural = _('Contracted teams')
